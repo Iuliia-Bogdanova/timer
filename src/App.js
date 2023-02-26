4b64686aeed14afeb5a76b19b07483836f6aff20
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-02-26 21:04:33 +0400

    until componentDidMount & localStorage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import React from "react";
+import icon from "./stopwatch.svg";
+import "./App.css";
 
-function App() {
+class App extends React.Component{
+  constructor(props)
+  {
+    super(props);
+      this.state = {
+        minutes: 0,
+        seconds: 0
+      }
+  }
+
+  render(){
   return (
-    <div className="App">
-      
-    </div>
-  );
-}
+      <div className="App">
+        <img src={icon} className='App-icon' alt="timer" />
+        <p>
+          Timer info: {this.state.minutes} : {this.state.seconds}
+        </p>
+      </div>
+    );
+  }
+} 
 
 export default App;
